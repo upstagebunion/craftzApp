9bdff9421d9cb6c68188175e12f11c18c5bc3784
upstagebunion <fco.garcia.solis@gmail.com>
2025-05-24 12:35:13 -0600

    Prueba despliegue puppeteer

--- backend/chromium-config.js
+++ backend/chromium-config.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 
 async function getLaunchOptions() {
   return {
-    args: chromium.args,
+    /*args: chromium.args,
     executablePath: process.env.CHROMIUM_PATH || await chromium.executablePath(),
-    headless: chromium.headless,
+    headless: chromium.headless,*/
+    headless: 'new',
     ignoreHTTPSErrors: true
   };
 }
